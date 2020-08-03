622b8a017aaf0c49cafb8627e43fcb79e1e0b305
mushding <ajy1005464@gmail.com>
2020-08-03 09:06:06 +0800

    fixed utf-8 problem

--- update_dbs/update_temporary_txt.py
+++ update_dbs/update_temporary_txt.py
@@ -21,7 +21,7 @@ sql_str = "delete from temporary_purpose"
 conn.execute(sql_str)
 conn.commit()
 
-with open("requirement.txt", "r") as fileHandler:  
+with open("add_new_classroom.txt", "r", encoding="utf-8") as fileHandler:  
     lines = fileHandler.readlines()
     for line in lines:
         if line.strip().split(' ')[0] != '':
