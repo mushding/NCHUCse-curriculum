d3063fdfb6fa3398b672b16ab09872ee5c7ada52
mushding <ajy1005464@gmail.com>
2020-09-09 22:33:30 +0800

    online_related add fixed db upload

--- online_related/update_online.py
+++ online_related/update_online.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import datetime
+import sqlite3
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
@@ -40,7 +42,26 @@ time_to_hour = {
     "A": "18",
     "B": "19",
     "C": "20",
+    "08": 8,
+    "09": 9,
+    "10": 10,
+    "11": 11,
+    "12": 12,
+    "13": 13,
+    "14": 14,
+    "15": 15,
+    "16": 16,
+    "17": 17,
+    "18": 18,
+    "19": 19,
+    "20": 20,
+    "21": 21,
+    "22": 22,
 }
+
+# connect to db
+dbfile = "../schedule-template/curriculum.db"
+conn = sqlite3.connect(dbfile)
 
 # init data list
 datas = list()
@@ -50,7 +71,6 @@ for i in range(7):
         "password": 3345678,
     })
 session = requests.Session()
-
 
 for career, postdata in enumerate(postdatas):
     def post_to_website(name, days, classroom, teacher, grade):
@@ -84,11 +104,19 @@ for career, postdata in enumerate(postdatas):
             else:
                 post_to_website(classes[2].split(" ")[0], classes[8].split(","), re.split('(\d+)', classes[10]), classes[12], index)
 
+# select data
+c = conn.cursor()
+sql_str = "select * from static_purpose"
+records = c.execute(sql_str)
+for row in records:
+    for time in range(time_to_hour[row[3][:2]], time_to_hour[row[4][:2]]):
+        datas[row[2] - 1]['application_' + str(row[5]) + "_" + str(row[2]) + "_" + str(time)] = (row[0] + "\r\n" + row[1] + "\r\n").encode("Big5")
+
+# post oneweek data to website
 for data in datas:
     print(data)
     resp = session.post('http://www.cs.nchu.edu.tw/class/update.php', headers = headers, data = data)
 
-data = {'id': 4, 'password': 3345678,'application_241_4_10': b'\xb4\xb6\xb3q\xaa\xab\xb2z\xbe\xc7\r\n\xbeG\xab\xd8\xa9v\r\n\xa4j\xa4@', 'application_241_4_11': b'\xb4\xb6\xb3q\xaa\xab\xb2z\xbe\xc7\r\n\xbeG\xab\xd8\xa9v\r\n\xa4j\xa4@', 'application_242_4_9': b'\xa7@\xb7~\xa8t\xb2\xce\r\n\xb1i\xb0a\xb1l\r\n\xa4j\xa4T', 'application_242_4_10': b'\xa7@\xb7~\xa8t\xb2\xce\r\n\xb1i\xb0a\xb1l\r\n\xa4j\xa4T', 'application_242_4_11': b'\xa7@\xb7~\xa8t\xb2\xce\r\n\xb1i\xb0a\xb1l\r\n\xa4j\xa4T', 'application_821_4_13': b'\xb0\xca\xbaA\xba\xf4\xad\xb6\xb5{\xa6\xa1\xb3]\xadp\r\n\xb6\xc0\xacK\xbf\xc4\r\n\xa4j\xa4T', 'application_821_4_14': b'\xb0\xca\xbaA\xba\xf4\xad\xb6\xb5{\xa6\xa1\xb3]\xadp\r\n\xb6\xc0\xacK\xbf\xc4\r\n\xa4j\xa4T', 'application_821_4_15': b'\xb0\xca\xbaA\xba\xf4\xad\xb6\xb5{\xa6\xa1\xb3]\xadp\r\n\xb6\xc0\xacK\xbf\xc4\r\n\xa4j\xa4T', 'application_335_4_14': b'\xb0\xcf\xb0\xec\xba\xf4\xb8\xf4\r\n\xb0\xaa\xb3\xd3\xa7U\r\n\xa4j\xa5|', 'application_335_4_15': b'\xb0\xcf\xb0\xec\xba\xf4\xb8\xf4\r\n\xb0\xaa\xb3\xd3\xa7U\r\n\xa4j\xa5|', 'application_335_4_16': b'\xb0\xcf\xb0\xec\xba\xf4\xb8\xf4\r\n\xb0\xaa\xb3\xd3\xa7U\r\n\xa4j\xa5|', 'application_336_4_14': b'\xb0\xaa\xb5\xa5\xb8\xea\xae\xc6\xb1\xb4\xb0\xc9\xbbP\xa5\xa8\xb6q\xb8\xea\xae\xc6\xa4\xc0\xaaR\r\n\xadS\xc4\xa3\xa4\xa4\xb5\xa5\r\n\xba\xd3\xa4h', 'application_336_4_15': b'\xb0\xaa\xb5\xa5\xb8\xea\xae\xc6\xb1\xb4\xb0\xc9\xbbP\xa5\xa8\xb6q\xb8\xea\xae\xc6\xa4\xc0\xaaR\r\n\xadS\xc4\xa3\xa4\xa4\xb5\xa5\r\n\xba\xd3\xa4h', 'application_336_4_16': b'\xb0\xaa\xb5\xa5\xb8\xea\xae\xc6\xb1\xb4\xb0\xc9\xbbP\xa5\xa8\xb6q\xb8\xea\xae\xc6\xa4\xc0\xaaR\r\n\xadS\xc4\xa3\xa4\xa4\xb5\xa5\r\n\xba\xd3\xa4h', 'application_338_4_14': b'\xb0\xaa\xb5\xa5\xb6W\xa4j\xab\xac\xbfn\xc5\xe9\xb9q\xb8\xf4\xb3]\xadp\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_338_4_15': b'\xb0\xaa\xb5\xa5\xb6W\xa4j\xab\xac\xbfn\xc5\xe9\xb9q\xb8\xf4\xb3]\xadp\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_338_4_16': b'\xb0\xaa\xb5\xa5\xb6W\xa4j\xab\xac\xbfn\xc5\xe9\xb9q\xb8\xf4\xb3]\xadp\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_1007_4_9': b'SOC\xb4\xfa\xb8\xd5\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_1007_4_10': b'SOC\xb4\xfa\xb8\xd5\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_1007_4_11': b'SOC\xb4\xfa\xb8\xd5\r\n\xb6\xc0\xbcw\xa6\xa8\r\n\xba\xd3\xa4h', 'application_1019_4_9': b'\xb5L\xbdu\xba\xf4\xb8\xf4\xae\xc4\xaf\xe0\xa4\xc0\xaaR\r\n\xb4\xbf\xbe\xc7\xa4\xe5\r\n\xba\xd3\xa4h', 'application_1019_4_10': b'\xb5L\xbdu\xba\xf4\xb8\xf4\xae\xc4\xaf\xe0\xa4\xc0\xaaR\r\n\xb4\xbf\xbe\xc7\xa4\xe5\r\n\xba\xd3\xa4h', 'application_1019_4_11': b'\xb5L\xbdu\xba\xf4\xb8\xf4\xae\xc4\xaf\xe0\xa4\xc0\xaaR\r\n\xb4\xbf\xbe\xc7\xa4\xe5\r\n\xba\xd3\xa4h', 'application_338_4_9': b'\xb8\xea\xae\xc6\xaew\xa8t\xb2\xce\r\n\xb8\xeb\xa9[\xaa\xda\r\n\xba\xd3\xa4h', 'application_338_4_10': b'\xb8\xea\xae\xc6\xaew\xa8t\xb2\xce\r\n\xb8\xeb\xa9[\xaa\xda\r\n\xba\xd3\xa4h', 'application_338_4_11': b'\xb8\xea\xae\xc6\xaew\xa8t\xb2\xce\r\n\xb8\xeb\xa9[\xaa\xda\r\n\xba\xd3\xa4h', 'application_803_4_18': b'\xb9\xcf\xa7\xce\xc3\xd1\xa7O\r\n\xb3\xeb\xa5\xdb\xa5\xcd\r\n\xba\xd3\xb1M', 'application_803_4_19': b'\xb9\xcf\xa7\xce\xc3\xd1\xa7O\r\n\xb3\xeb\xa5\xdb\xa5\xcd\r\n\xba\xd3\xb1M', 'application_803_4_20': b'\xb9\xcf\xa7\xce\xc3\xd1\xa7O\r\n\xb3\xeb\xa5\xdb\xa5\xcd\r\n\xba\xd3\xb1M'}
-session.post('http://www.cs.nchu.edu.tw/class/update.php', headers = headers, data = data)
-
-print(datas[3])
+# post today again (bug)
+session.post('http://www.cs.nchu.edu.tw/class/update.php', headers = headers, data = datas[datetime.datetime.today().weekday()])
+print(datas[datetime.datetime.today().weekday()])
---
+++ online_related/雙擊更新網站.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001
+python clear_online.py
+python update_online.py
+echo.
+echo ===========================
+echo 網站更新完成！
+echo ===========================
+echo.
+pause
