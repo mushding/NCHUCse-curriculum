022c46abd81f08f71fe33413b29c336b1da897cf
mushding <ajy1005464@gmail.com>
2020-10-16 13:26:59 +0800

    fix bug: one hour class bug

--- server/router.js
+++ server/router.js
@@ -4,24 +4,40 @@ import DB from './db/curriculum';
 
 const router = express.Router();
 
-const timestamps = {
-    "1": 8,
-    "2": 9,
-    "3": 10,
-    "4": 11,
-    "5": 13,
-    "6": 14,
-    "7": 15,
-    "8": 16,
-    "9": 17,
-    "10": 18,
-    "11": 19,
-    "12": 20,
-    "13": 21,
-    "14": 22,
-    "A": 18,
-    "B": 19,
-    "C": 20,
+const startTimestamps = {
+    "1": "08",
+    "2": "09",
+    "3": "10",
+    "4": "11",
+    "5": "13",
+    "6": "14",
+    "7": "15",
+    "8": "16",
+    "9": "17",
+    "A": "18",
+    "B": "19",
+    "C": "20",
+    "D": "21",
+    "E": "22",
+    "F": "23",
+}
+
+const endTimestamps = {
+    "1": "09",
+    "2": "10",
+    "3": "11",
+    "4": "12",
+    "5": "14",
+    "6": "15",
+    "7": "16",
+    "8": "17",
+    "9": "18",
+    "A": "19",
+    "B": "20",
+    "C": "21",
+    "D": "22",
+    "E": "23",
+    "F": "24",
 }
 
 const weekIndex = {
@@ -43,13 +59,12 @@ router.get('/getWebsite/:classroom', async (req, res) => {
         res.sendStatus(500);
     }
     for (let i = 0; i < result.length; i++){
-        let start_time = timestamps[result[i]["time"][0]];
-        let end_time = timestamps[result[i]["time"].slice(-1)];
-        end_time += 1;
+        let start_time = startTimestamps[result[i]["time"][0]];
+        let end_time = endTimestamps[result[i]["time"].slice(-1)];
         curriculum.push({
             title: result[i]["name"] + "\n" + result[i]["grade"] + "\n" + result[i]["teacher"],
-            startDate: '2020-09-01T' + String(start_time) + ":00",
-            endDate: '2020-09-01T' + String(end_time) + ":00",
+            startDate: '2020-09-01T' + start_time + ":00",
+            endDate: '2020-09-01T' + end_time + ":00",
             rRule: 'RRULE:FREQ=WEEKLY;COUNT=18;WKST=MO;BYDAY=' + weekIndex[result[i]["week"]],
             addtime: result[i]["timestamp"],
             name: result[i]["name"],
