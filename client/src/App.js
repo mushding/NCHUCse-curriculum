21654b94f764f4751ccb114e17106ffe9849ba93
mushding <ajy1005464@gmail.com>
2020-10-16 13:28:27 +0800

    fix bug: one hour class bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashBoard from './Pages/DashBoard/DashBoard';
+import DashBoard from './DashBoard/DashBoard';
 
 export default class App extends React.Component{
     render(){
