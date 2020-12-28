b19fb7aba8f380a49e0bed89587bc828425d098b
mushding <ajy1005464@gmail.com>
2020-12-28 11:37:31 +0800

    add mysql backup

---
+++ mysql-docker-backup.sh
@@ -0,0 +1,10 @@
+UPPERCASE=${PWD##*/}
+CONTAINER=${UPPERCASE,,}
+
+. .env
+
+# Backup
+docker exec ${CONTAINER}_db_1 mysqldump -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -c > "$(pwd)/backup/backup-`date +%Y-%m-%d`.sql.gz"
+
+# Restore
+# cat backup.sql | docker exec -i ${CONTAINER}_db_1 /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
