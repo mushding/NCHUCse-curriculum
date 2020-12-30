d873c40d703b04bf253cc60f51a9fe86871cb855
mushding <ajy1005464@gmail.com>
2020-12-30 09:28:06 +0800

    fix crontab path bug

--- mysql-docker-backup.sh
+++ mysql-docker-backup.sh
@@ -4,7 +4,7 @@ CONTAINER=${UPPERCASE,,}
 . .env
 
 # Backup
-docker exec ${CONTAINER}_db_1 mysqldump -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -c > "$(pwd)/backup/backup-`date +%Y-%m-%d`.sql.gz"
+docker exec ${CONTAINER}_db_1 mysqldump -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -c > "${HOME}/Desktop/NCHUCse-curriculum/backup/backup-`date +%Y-%m-%d`.sql.gz"
 
 # Restore
 # cat backup.sql | docker exec -i ${CONTAINER}_db_1 /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
