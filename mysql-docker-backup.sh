13a1e6bc11ab2130201a673bcc2173946f432cf0
mushding <ajy1005464@gmail.com>
2020-12-31 11:15:45 +0800

    fix crontab but

--- mysql-docker-backup.sh
+++ mysql-docker-backup.sh
@@ -1,10 +1,12 @@
-UPPERCASE=${PWD##*/}
-CONTAINER=${UPPERCASE,,}
+CONTAINER="nchucse-curriculum_db_1"
+REPO="${HOME}/Desktop/NCHUCse-curriculum"
+
+cd ${REPO}
 
 . .env
 
 # Backup
-docker exec ${CONTAINER}_db_1 mysqldump -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -c > "${HOME}/Desktop/NCHUCse-curriculum/backup/backup-`date +%Y-%m-%d`.sql.gz"
+docker exec ${CONTAINER} mysqldump -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -c > "${REPO}/backup/backup-`date +%Y-%m-%d`.sql.gz" > /var/log/NCHUCse_curriculum.log 2>&1
 
 # Restore
-# cat backup.sql | docker exec -i ${CONTAINER}_db_1 /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
+# cat backup.sql | docker exec -i ${CONTAINER} /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
